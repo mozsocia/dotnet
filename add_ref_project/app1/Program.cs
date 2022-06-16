a960208bf527e93d9e3333a593681534cc2f1316
mozdalif <mozsocia@gmail.com>
2022-06-16 12:14:29 +0600

    Revert "Add files via upload"
    This reverts commit 3c26aa1b64d6e49170edf40ea882955758f9718e.

--- add_ref_project/app1/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using app2;
-
-Class1 c = new Class1();
-
-Console.WriteLine(c.GetString());
-Console.WriteLine("Hello, World!");
--- add_ref_project/app2/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace app2;
-public class Class1
-{
-    public string GetString()
-    {
-        return "Hello World this is app2";
-    }
-}
