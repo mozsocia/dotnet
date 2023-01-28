f42794d4a42a2a85a2b14745d5b6739f00cfbd14
mozdalif <mozsocia@gmail.com>
2023-01-28 18:50:53 +0600

    sqlite console app

---
+++ ef-core_console_app_sqlite/MyApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyApp {
+    public class Program {
+        public static void Main(string[] args) {
+            using (var context = new SqliteDbContext()) {
+
+                // Start with a clean database
+                // context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                // Add reminders.
+                context.Reminders.Add(new Reminder {
+                    Title = "Meditate"
+                });
+                context.Reminders.Add(new Reminder {
+                    Title = "Eat a nutritious breakfast"
+                });
+                context.SaveChanges();
+
+                // Fetch Reminders
+                var reminders = context.Reminders.ToArray();
+                foreach(var reminder in reminders) {
+                    Console.WriteLine($"{reminder.Title}");
+                }
+
+          
+              
+            }
+        }
+    }
+}
